1223f55abf15c445b62c278b75e5a590843c8443
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-06-02 10:48:21 -0700

    Adding Navigation

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 A boilerplate for the Aurelia framework based on the tutorial at http://aurelia.io/docs.html#/aurelia/framework/1.0.0-beta.1.2.4/doc/article/getting-started
 
 - Creating Your First Screen
+- Adding Navigation
--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
-export class Welcome {
-  heading = 'Welcome to Aurelia!';
-  firstName = 'John';
-  lastName = 'Doe';
-
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  
-  submit() {
-    alert(`Welcome, ${this.fullName}!`);
-  }
+export class App {
+	configureRouter(config, router) {
+		config.title = 'Aurelia';
+		config.map([
+			{ route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' }			
+		]);
+		this.router = router;
+	}
 }
